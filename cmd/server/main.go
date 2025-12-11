51631ed8efad32131b2393c4567eda129b6798dc
Egor Chuprov <ytdrenk@gmail.com>
2025-12-11 18:03:58 +0300

    first server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
-package server
+package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		panic(err)
+	}
 }
